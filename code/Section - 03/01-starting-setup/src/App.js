b00d1dc4fed9a4ea798fd84fb94a7be7cfac4775
Hariomsingh333 <www.hariom03.com@gmail.com>
2022-05-31 20:21:18 +0530

    change the name of src to code and add our first component to s3 project

---
+++ code/Section - 03/01-starting-setup/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+const App = () => {
+  return (
+    <div>
+      hello world
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
