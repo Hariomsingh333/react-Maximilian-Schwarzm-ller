1888a6fa8292735889a9bc711387ed3d76068d53
Hariomsingh333 <www.hariom03.com@gmail.com>
2022-06-01 20:46:23 +0530

    add more props in ExpenseItem component

--- code/Section - 03/01-starting-setup/src/App.js
+++ code/Section - 03/01-starting-setup/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
-import Test from "./components/Test";
+// import Test from "./components/Test";
 
 const App = () => {
   // static data to render via props
@@ -32,6 +32,21 @@ const App = () => {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
       />
       {/* <Test text={`this is a text from props`} /> */}
     </div>
