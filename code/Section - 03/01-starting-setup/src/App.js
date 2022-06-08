172e33aa6996d472fde22b2e3e6d3aec4b8b6d1a
Hariomsingh333 <www.hariom03.com@gmail.com>
2022-06-08 19:55:10 +0530

    add form data to the array list

--- code/Section - 03/01-starting-setup/src/App.js
+++ code/Section - 03/01-starting-setup/src/App.js
@@ -1,46 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./components/Card";
 // import ExpenseItem from "./components/Expense/ExpenseItem";
 import Expenses from "./components/Expense/Expenses";
 import NewExpense from "./components/Form/NewExpense";
 // import Test from "./components/Test";
 
+// static data to render via props
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(),
+  },
+];
 const App = () => {
-  // static data to render via props
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(),
-    },
-  ];
-
   // take the data from NewExpense component
   const newExpenseData = (newExpenseData) => {
     const ExpenseData = {
       ...newExpenseData,
     };
     console.log(ExpenseData);
+    setExpense((preExpense) => {
+      return [ExpenseData, ...preExpense];
+    });
   };
+  const [Expense, setExpense] = useState(dummyExpenses);
   return (
     <div>
       <Card className={`expenses`}>
         <NewExpense ExpenseDataHandler={newExpenseData} />
-        <Expenses items={expenses} />
+        <Expenses items={Expense} />
       </Card>
     </div>
   );
