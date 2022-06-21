8953c913d3d17b4e9adebc846d7668015aa349a2
Hariomsingh333 <www.hariom03.com@gmail.com>
2022-06-21 15:23:25 +0530

    fix some jsx classname in Cart.jsx

--- code/Section - 11/order-food-app/src/components/Cart/Cart.jsx
+++ code/Section - 11/order-food-app/src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = (
-    <ul className={classes["cart-item"]}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: 2, price: 12 }].map((item) => (
         <li>{item.name}</li>
       ))}
@@ -17,7 +17,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>35</span>
       </div>
-      <div className={classes.action}>
+      <div className={classes.actions}>
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
